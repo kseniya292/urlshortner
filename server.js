84283b86bca33c58a79883248f56d92d898f7817
Kseniya Hogan <kseniya.hogan@gmail.com>
2016-11-09 13:58:44 -0500

    finished app

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var mongodb = require('mongodb');
+app.set('port', (process.env.PORT || 5000));
 
 var app = express();
 var MongoClient = mongodb.MongoClient;
 
 //(Focus on This Variable)
-var url = 'mongodb://kseniya292:Anas5Url@ds141937.mlab.com:41937/urlshortner';      
+// var url = 'mongodb://kseniya292:Anas5Url@ds141937.mlab.com:41937/urlshortner'; //change to local 
+var localURL = "mongodb://root:password@localhost:27017/urlshortnerLOCALDB"  ;   
+app.set('MONGO_URL', (process.env.MONGO || localURL))
 //(Focus on This Variable)
 
 function idShortner() {
@@ -47,11 +50,11 @@ app.get('/:parameter', function (req, res) {
 		// create store original and short URL
 			function storeShortUrl(passedUrl) {
 			// Use connect method to connect to the Server
-				MongoClient.connect(url, function (err, db) {
+				MongoClient.connect(app.get('MONGO_URL'), function (err, db) {
 				  if (err) {
 				    console.log('Unable to connect to the mongoDB server. Error:', err);
 				  } else {
-				    console.log('Connection established to', url);
+				    console.log('Connection established to', app.get('MONGO_URL'));
 				    // console.log(short_url);
 				    db.collection('urlshortner').insert({"original_url" : passedUrl, "short_url" : short_url}, function (err, data) {
 				    	if (err) throw err;
@@ -74,18 +77,18 @@ app.get('/:parameter', function (req, res) {
 
 }); //app.get
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
 });
 
 
 function queryDB (checkURL, gotData) {
 		
-	MongoClient.connect(url, function (err, db) {
+	MongoClient.connect(app.get('MONGO_URL'), function (err, db) {
 	  if (err) {
 	    console.log('Unable to connect to the mongoDB server. Error:', err);
 	  } else {
-	    console.log('Connection established to', url);
+	    console.log('Connection established to', app.get('MONGO_URL'));
 
 	    db.collection('urlshortner').findOne({short_url : checkURL}, { original_url : 1, _id : 0}, gotData); //db colletions
 	     
